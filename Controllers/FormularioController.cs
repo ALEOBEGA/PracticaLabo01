888f684958faa36b679da22e59ba92a5ff9dc3a9
Alex Berrocal <alex_berrocal@usmp.pe>
2020-10-18 15:28:34 -0500

    Practicanumero1

---
+++ Controllers/FormularioController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PracticaLabo01.Models;
+
+namespace PracticaLabo01.Controllers
+{
+    public class FormularioController : Controller
+    {
+        private readonly ILogger<FormularioController> _logger;
+
+        public FormularioController(ILogger<FormularioController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+
+         [HttpPost]
+        public IActionResult Formulario( Formulario objFormulario){
+         if(ModelState.IsValid){
+             objFormulario.Respuesta="Se registraron sus datos en la Base de Datos.";
+         }
+         return View("Formulario",objFormulario);
+        }
+
+    }
+}
+
