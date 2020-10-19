96d8900a65afa06890b1213f80d7c86321db4c9c
Alex Berrocal <alex_berrocal@usmp.pe>
2020-10-18 23:19:35 -0500

    Case Sensitive

--- Models/Formulario.cs
+++ Models/Formulario.cs
@@ -12,27 +12,37 @@ namespace PracticaLabo01.Models
 
         [Required(ErrorMessage="Por favor ingrese Datos")] 
         [Display(Name="Nombre")]  
+        [Column("nombre")] 
         public string Nombre { get; set; }
         
         [Required(ErrorMessage="Por favor ingrese Datos")]  
         [Display(Name="Apellido")]
+        [Column("apellido")] 
         public string Apellido { get; set; }
         
         [Required(ErrorMessage="Por favor ingrese Datos")]  
         [Display(Name="Distrito")]
+        [Column("distrito")] 
         public String Distrito { get; set; }
 
         [Required(ErrorMessage="Por favor ingrese Datos")]  
         [Display(Name="Bancos")]
+        [Column("banco")] 
         public string Bancos { get; set; }
 
         [Required(ErrorMessage="Por favor ingrese Datos")]  
         [Display(Name="Edad")]
+        [Column("edad")] 
         public string Edad { get; set; }
+
+        [Required(ErrorMessage="Por favor seleccione el Dato")]  
+        [Display(Name="Genero")]
+        [Column("genero")] 
         public string Genero { get; set; }
 
         [Required(ErrorMessage="Por favor ingrese Datos")]  
         [Display(Name="Autor")]
+        [Column("autor")] 
         public string Autor { get; set; }
 
         [Display(Name="Output")] 
